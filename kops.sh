216b0299f5efbaa68eec5e6f814a5d94cd49625f
Prasad2390 <vanacharlavaraprasad@gmail.com>
2025-06-20 11:05:04 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -16,8 +16,18 @@ curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s htt
 chmod +x kops
 sudo mv kops /usr/local/bin/kops
 
+
+#kops
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+#kubectl
+ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+ chmod +x kubectl 
+ mv kubectl /usr/local/bin
+
 aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+export KOPS_STATE_STORE=s3://prasad.flmk8s
+kops create cluster --name prasad.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --master-volume-size 30 --node-count=2 --node-size t2.medium --node-volume-size 20 --image=ami-0a7d80731ae1b2435 --yes
 kops update cluster --name rahams.k8s.local --yes --admin
