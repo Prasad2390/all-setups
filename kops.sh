2aaf21011b53c0af4cbe198039778577b6480f3a
Prasad2390 <vanacharlavaraprasad@gmail.com>
2026-01-05 11:01:23 +0000

    Fix KOPS_STATE_STORE bucket name format

--- kops.sh
+++ kops.sh
@@ -12,7 +12,7 @@ mv kubectl /usr/local/bin/kubectl
 
 aws s3api create-bucket --bucket prasad..flm.k8s --region us-east-1
 aws s3api put-bucket-versioning --bucket mustafa.project.flm.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://prasad..flm.k8s
+export KOPS_STATE_STORE=s3://prasad.flm.k8s
 kops create cluster --name prasad.k8s.local --zones us-east-1a,us-east-1b --master-count=1 --master-size c7i-flex.large --master-volume-size=25 --node-count=2 --node-size t2.small --node-volume-size=20
 kops update cluster --name prasad.k8s.local --yes --admin
 kops delete cluster prasad.k8s.local --yes
